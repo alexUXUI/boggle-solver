59c79edce4f3eb6b5af18c66cf03913bdabc0b86
Alex Bennett <alexandergraybennett@gmail.com>
2023-01-22 09:11:30 -0700

    clean up and documenting

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
-// use js_sys::Math;
-// use rand::{thread_rng, Rng};
-// use wasm_bindgen::prelude::*;
-
+/**
+ * Given a string of chars and a board size, generate a board from the string
+ * using the size as the length and width of the board matrix.
+ */
 pub fn generate_board_from_string(string: String, board_size: usize) -> Vec<Vec<char>> {
     let mut board: Vec<Vec<char>> = Vec::new();
     let mut row: Vec<char> = Vec::new();
@@ -28,45 +28,10 @@ fn test_generate_board_from_string() {
     assert_eq!(board, expected_board);
 }
 
-// struct Board {
-//     data: Vec<Vec<char>>,
-// }
-
-// impl Board {
-//     fn get(&self) -> &Vec<Vec<char>> {
-//         &self.data
-//     }
-
-//     fn set(&mut self, board: Vec<Vec<char>>) {
-//         self.data = board;
-//     }
-// }
-
-// const BOARD: Board = Board { data: Vec::new() };
-
-// fn random_char() -> char {
-//     let mut rng = thread_rng();
-//     rng.gen_range('a'..='z')
-// }
-
-// pub fn generate_board(board_size: u8) -> Vec<Vec<char>> {
-//     // provision a vector to hold the board
-//     let mut board: Vec<Vec<char>> = Vec::new();
-
-//     // generate a random board
-//     for i in 0..board_size {
-//         let mut row = Vec::new();
-
-//         for _ in 0..board_size {
-//             row.push(random_char());
-//         }
-
-//         board.push(row);
-//     }
-
-//     board
-// }
-
+/**
+ * Generate a fixed board for testing purposes
+ * this board has over 200 words given large dictionarys
+ */
 pub fn generate_fixed_board() -> Vec<Vec<char>> {
     let mut board: Vec<Vec<char>> = Vec::new();
 
@@ -84,63 +49,3 @@ pub fn generate_fixed_board() -> Vec<Vec<char>> {
 
     board
 }
-
-// #[wasm_bindgen]
-// pub fn gerenate_board_string() -> String {
-//     let mut board_string = String::new();
-
-//     let consonants = vec![
-//         'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W',
-//         'X', 'Y', 'Z',
-//     ];
-
-//     for _ in 0..5 {
-//         board_string.push('E');
-//     }
-
-//     for _ in 0..2 {
-//         board_string.push('I');
-//     }
-
-//     for _ in 0..2 {
-//         board_string.push('A');
-//     }
-
-//     for _ in 0..2 {
-//         board_string.push('T');
-//     }
-
-//     board_string.push('C');
-//     board_string.push('N');
-//     board_string.push('R');
-//     board_string.push('S');
-//     board_string.push('B');
-//     board_string.push('D');
-//     board_string.push('H');
-
-//     // add the rest of the letters
-//     for _ in 0..(25 - board_string.len()) {
-//         let random_number = Math::random();
-//         let random_number = random_number * 21.0;
-//         let random_number = random_number.floor();
-//         let random_number = random_number as u32;
-
-//         board_string.push(consonants[random_number as usize]);
-//     }
-
-//     // reorder the string randomly
-//     let mut new_board_string = String::new();
-
-//     for _ in 0..25 {
-//         let random_number = Math::random();
-//         let random_number = random_number * board_string.len() as f64;
-//         let random_number = random_number.floor();
-//         let random_number = random_number as u32;
-
-//         new_board_string.push(board_string.remove(random_number as usize));
-//     }
-
-//     // make the string lowercase
-//     new_board_string = new_board_string.to_lowercase();
-//     new_board_string
-// }
--- src/trie.rs
+++ src/trie.rs
@@ -8,7 +8,6 @@ pub struct TrieNode {
 }
 
 impl TrieNode {
-    // Create new node
     pub fn new(c: char, is_final: bool) -> TrieNode {
         TrieNode {
             value: Option::Some(c),
@@ -25,11 +24,6 @@ impl TrieNode {
         }
     }
 
-    // Check if a node has that value
-    pub fn check_value(self, c: char) -> bool {
-        self.value == Some(c)
-    }
-
     pub fn insert_value(&mut self, c: char, is_final: bool) {
         self.child_nodes.insert(c, TrieNode::new(c, is_final));
     }
@@ -41,7 +35,6 @@ pub struct TrieStruct {
 }
 
 impl TrieStruct {
-    // Create a TrieStruct
     pub fn create() -> TrieStruct {
         TrieStruct {
             root_node: TrieNode::new_root(),
@@ -50,36 +43,54 @@ impl TrieStruct {
 
     // Insert a string
     pub fn insert(&mut self, string_val: String) {
+        // starting at the root node
         let mut current_node = &mut self.root_node;
+        // convert the string to a list of characters
         let char_list: Vec<char> = string_val.chars().collect();
+        // pointer to most recent match
         let mut last_match = 0;
-
+        // for the length of the string
         for letter_counter in 0..char_list.len() {
+            // if the current node has a child node with the current character
             if current_node
                 .child_nodes
                 .contains_key(&char_list[letter_counter])
             {
+                // set the current node to the child node
                 current_node = current_node
                     .child_nodes
                     .get_mut(&char_list[letter_counter])
                     .unwrap();
             } else {
+                // if the current node does not have a child node with the current character
                 last_match = letter_counter;
+
+                // break out of the loop
                 break;
             }
+            // and continue the loop to the next character
             last_match = letter_counter + 1;
         }
 
+        // if the last match is the length of the string
+        // set the current node to final
         if last_match == char_list.len() {
             current_node.is_final = true;
         } else {
+            // else insert the rest of the new string into the trie
+            // starting from the last match
             for new_counter in last_match..char_list.len() {
+                // recursively insert the rest of the new string
                 current_node.insert_value(char_list[new_counter], false);
+
+                // set the current node to the child node
                 current_node = current_node
                     .child_nodes
                     .get_mut(&char_list[new_counter])
                     .unwrap();
             }
+            // once the rest of the string is inserted
+            // set the current node to final
             current_node.is_final = true;
         }
     }
@@ -98,6 +109,7 @@ impl TrieStruct {
                     .unwrap();
             }
         }
+        // is final check to determine if the word is in the trie
         return current_node.is_final;
     }
 
